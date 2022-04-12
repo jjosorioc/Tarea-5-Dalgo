a21f75f8049b03a4a154fa5876b92988814d6271
Esteban Gonzalez Ruales <74991415+est-gonzalezr@users.noreply.github.com>
2022-04-11 21:33:36 -0500

    Implement some graph methods

--- Tarea 5/src/Graph.java
+++ Tarea 5/src/Graph.java
@@ -1,4 +1,5 @@
 import java.util.Map;
+import java.util.Set;
 import java.util.HashMap;
 
 public class Graph {
@@ -10,5 +11,24 @@ public class Graph {
 		this.vertices = new HashMap<Integer, Boolean>();
 		for (int i = 0; i < this.adjMatrix.length; i++)
 			this.vertices.put(i, false);
+	}
+
+	public int getEdgeWeight(int startVertex, int endVertex) {
+		int value = 0;
+		if (startVertex > this.adjMatrix.length - 1 || endVertex > this.adjMatrix.length - 1)
+			value = Integer.MIN_VALUE;
+		else
+			value = adjMatrix[startVertex][endVertex];
+
+		return value;
+	}
+
+	public Set<Integer> getVertices() {
+		return vertices.keySet();
+	}
+
+	public void markVertex(int vertex) {
+		if (vertices.containsKey(vertex))
+			vertices.put(vertex, true);
 	}
 }
